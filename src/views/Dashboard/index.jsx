1f0dd3b83e6a4ebc1043404b7ffaacc98abfc37c
Hugo Santos <hf.santos2332@gmail.com>
2023-07-01 21:54:56 -0300

    Implementa dashboard

--- src/views/Dashboard/index.jsx
+++ src/views/Dashboard/index.jsx
@@ -1,7 +1,79 @@
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './dashboards.css';
+
 const Dashboard = () => {
+  const [roomsData, setRoomsData] = useState([
+   //{{ id: 1, name: 'Sala 1', status: 'Ocupada', borrower: 'João', time: '10:00', room: "0" }}
+    { id: 1, name: 'Sala 1', status: 'Disponível', room: "1" },
+    { id: 2, name: 'Sala 2', status: 'Disponível', room: "2" },
+    { id: 3, name: 'Sala 3', status: 'Disponível', room: "3" },
+    { id: 4, name: 'Sala 4', status: 'Disponível', room: "4" },
+    { id: 5, name: 'Sala 5', status: 'Disponível', room: "5" },
+    { id: 6, name: 'Sala 6', status: 'Disponível', room: "6" },
+    { id: 7, name: 'Sala 7', status: 'Disponível', room: "7" },
+    { id: 8, name: 'Sala 8', status: 'Disponível', room: "8" },
+  ]);
+
+  const fetchEmprestimos = async () => {
+    try{
+      const response = await fetch(
+        `http://${import.meta.env.VITE_API_HOST}:${import.meta.env.VITE_API_PORT}/emprestimos/pendings?comProfessor=true`
+      );
+      const emprestimos = await response.json();
+
+      emprestimos.forEach(emprestimo => {
+        const { horarioEmprestimo, sala, professor: { nome } } = emprestimo;
+
+        const newRoomsData = roomsData.map(roomData => {
+          console.log(roomData.room, sala);
+          if (roomData.room === sala) {
+            roomData.status = 'Ocupada';
+            roomData.borrower = nome;
+            roomData.time = new Date(horarioEmprestimo).toLocaleString('pt-BR');
+          }
+
+          return roomData;
+        })
+        console.log(newRoomsData);
+        setRoomsData(newRoomsData);
+      });
+    }catch(error) {
+      console.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchEmprestimos();
+  }, []);
+
+
+
   return (
-    <div>
-      Dashboard
+    <div className="container">
+      <h1>Dashboard de Salas</h1>
+      <div className="row">
+        {roomsData.map(room => (
+          <div key={room.id} className="col-lg-3 col-md-4 col-sm-6">
+            <div className={`card ${room.status !== 'Ocupada' ? 'text-success' : ''}`}>
+              <div className="card-body">
+                <h5 className={`card-title ${room.status !== 'Ocupada' ? 'text-success' : ''}`}>
+                  {room.name}
+                </h5>
+                <p className={`card-text ${room.status !== 'Ocupada' ? 'text-success' : ''}`}>
+                  {room.status}
+                </p>
+                {room.status === 'Ocupada' && (
+                  <div>
+                    <p>Emprestado para: {room.borrower}</p>
+                    <p>Horário: {room.time}</p>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
